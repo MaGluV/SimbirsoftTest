53a771765518e8ab4e52dade8716477fe0ae0937
Max Glushkov <sh345sqrt@gmail.com>
2021-07-22 22:23:28 +0300

    Sub-optimal XPATHs was changed

--- locators.py
+++ locators.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 class MainPageLocators:
-	login_button_xpath = "/html/body/div/div/div[2]/div/div/div[4]/a[2]"
-	
+    login_button_xpath = "//a[@class='control button2 button2_view_classic button2_size_mail-big button2_theme_mail-white button2_type_link HeadBanner-Button HeadBanner-Button-Enter with-shadow']"
+
 class LoginLocators:
-	login_id = "passp-field-login"
-	login_button_xpath = "/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[3]/button"
-	password_id = "passp-field-passwd"
-	password_button_xpath = "/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[3]/button"
-	
+    login_id = "passp-field-login"
+    login_button_xpath = "//button[@id='passp:sign-in']"
+    password_id = "passp-field-passwd"
+
 class PersonalPageLocators:
-	email_subjects_xpath = "//span[@class='mail-MessageSnippet-Item mail-MessageSnippet-Item_subjectWrapper js-message-snippet-subject']/*"
-	mail_addresses_xpath = "//span[@class='mail-MessageSnippet-FromText']"
-	mail_addresses_attribute = 'title'
-	
+    email_subjects_xpath = "//span[@class='mail-MessageSnippet-Item mail-MessageSnippet-Item_subjectWrapper js-message-snippet-subject']/*"
+    mail_addresses_xpath = "//span[@class='mail-MessageSnippet-FromText']"
+    mail_addresses_attribute = 'title'
+
 class SendingLetterLocators:
-	write_letter_button_xpath = "//a[@title='Написать (w, c)']"
-	addressee_xpath = "//div[@class='composeYabbles']"
-	email_subject_xpath = "//input[@class='composeTextField ComposeSubject-TextField']"
-	message_xpath = "//div[@class='cke_wysiwyg_div cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr cke_htmlplaceholder']"
-	send_message_button_xpath = "//button[@class='control button2 button2_view_default button2_tone_default button2_size_l button2_theme_normal button2_pin_circle-circle ComposeControlPanelButton-Button ComposeControlPanelButton-Button_sidePadding']"
+    write_letter_button_xpath = "//a[@title='Написать (w, c)']"
+    addressee_xpath = "//div[@class='composeYabbles']"
+    email_subject_xpath = "//input[@class='composeTextField ComposeSubject-TextField']"
+    message_xpath = "//div[@class='cke_wysiwyg_div cke_reset cke_enable_context_menu cke_editable cke_editable_themed cke_contents_ltr cke_htmlplaceholder']"
+    send_message_button_xpath = "//div[@class='new__root--3qgLa ComposeControlPanel-Button ComposeControlPanel-Button_new ComposeControlPanel-SendButton ComposeSendButton ComposeSendButton_desktop']/button"
