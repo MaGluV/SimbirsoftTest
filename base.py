6439d47eee9787b279781784b3e95fd895c370f0
Max Glushkov <sh345sqrt@gmail.com>
2021-07-22 22:20:16 +0300

    Remove comments and magical numbers

--- base.py
+++ base.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import pytest
 import allure
 from selenium import webdriver
@@ -8,24 +5,21 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class Base:
 
-	@pytest.fixture(autouse=True)
-	def set_up(self):
-		print("Initiating Firefox driver")
-		self.driver = webdriver.Remote(
-			command_executor='http://127.0.0.1:4444/wd/hub',
-			desired_capabilities={
-				'browserName':'firefox',
-				'javascriptEnabled':True})
-		print("Test is started")
-		self.driver.implicitly_wait(10)
-		self.driver.get("https://mail.yandex.ru/")
-		self.driver.maximize_window()
+    @pytest.fixture(autouse=True)
+    def set_up(self):
+        self.TEST_TIMEOUT = 20
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities={
+                'browserName':'firefox',
+                'javascriptEnabled':True})
+        self.driver.implicitly_wait(self.TEST_TIMEOUT)
+        self.driver.get("https://mail.yandex.ru/")
+        self.driver.maximize_window()
 		
-		yield self.driver
-		if self.driver is not None:
-			print("-----------------------------------------")
-			print("Tests is finished")
-			self.driver.close()
+        yield self.driver
+        if self.driver is not None:
+            self.driver.close()
 
 	
 	
